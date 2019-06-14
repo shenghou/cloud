3e9fe9e37982f7d886cd4885d6ad438b33e6d11c
侯胜 <hou993268@163.com>
2019-06-14 23:03:33 +0800

    py list & tuple

---
+++ packages/python/2_list/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+# hou = 'lomean'
+# h = hou[1]
+# print(h)
+
+
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = number[3:6]
+# b = number[0:1]
+# c = number[3:]
+# d = number[:6]
+# e = number[-3:]
+# f = number[:]
+# g = number[0:10:2]
+# h = number[0:10:-2]
+# i = number[10:0:2]
+# j = number[10:0:-2]
+# print(a, 'b', b, 'c', c, 'd', d, 'e', e,
+#       'f', f, 'g', g, 'h', h, 'i', i, 'j', j)
+
+
+# a = [1,2,3] + [4,5,6]
+# print(a)
